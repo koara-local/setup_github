2e53c3d535d67b1fb39962ffb3eb2188e4f2d045
Kohei Arao <koara.local.jp@gmail.com>
2017-01-30 12:55:08 +0900

    Create setup_github.sh

---
+++ setup_github.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+if [ ! -e ~/.ssh/id_rsa_gitlab ]; then
+    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa_gitlab
+fi
+
+if [ ! -e ~/.ssh/config ] || [ $(cat ~/.ssh/config | grep github | wc -l) == 0 ]; then
+cat << EOS >> ~/.ssh/config
+Host github github.com
+User git
+Hostname github.com
+Port 22
+IdentityFile ~/.ssh/id_rsa_github
+EOS
+fi
+
+echo -e "please, Adding a new SSH key to your GitHub account\n"
+
+cat ~/.ssh/id_rsa_github.pub
