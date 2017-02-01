bbf50c61f7a94ba413f366098f09807c3a8e638c
Kohei Arao <koara.local.jp@gmail.com>
2017-02-01 14:21:35 +0900

    Update setup_github.sh

--- setup_github.sh
+++ setup_github.sh
@@ -1,7 +1,13 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
 if [ ! -e ~/.ssh/id_rsa_github ]; then
-    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa_github
+    ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa_github
+else
+    echo -e "\"~/.ssh/id_rsa_github\" already exists."
+    echo -e "If you want to overwrite those files, delete them manually."
+    echo -e "\n"
+    echo -e "\"rm ~/.ssh/id_rsa_github ~/.ssh/id_rsa_github.pub\""
+    echo -e "\n"
 fi
 
 if [ ! -e ~/.ssh/config ] || [ $(cat ~/.ssh/config | grep github | wc -l) == 0 ]; then
@@ -14,6 +20,7 @@ IdentityFile ~/.ssh/id_rsa_github
 EOS
 fi
 
-echo -e "please, Adding a new SSH key to your GitHub account\n"
+echo -e "please, Adding a new SSH key to your GitHub account.\n"
+echo -e "https://github.com/settings/ssh\n"
 
 cat ~/.ssh/id_rsa_github.pub
