9348da80e697cc969d89c1ea5f6b2dcb7f160b5b
Kohei Arao <koara.local.jp@gmail.com>
2017-02-01 15:45:29 +0900

    Update setup_github.sh

--- setup_github.sh
+++ setup_github.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 if [ ! -e ~/.ssh/id_rsa_github ]; then
-    ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa_github
+    read -sp "Enter passphrase (minimum five characters) [empty for no passphrase]:" PASSPHRASE
+    echo ""
+    ssh-keygen -t rsa -b 2048 -N "${PASSPHRASE}" -f ~/.ssh/id_rsa_github
 else
     echo "\"~/.ssh/id_rsa_github\" already exists."
     echo "If you want to overwrite those files, delete them manually."
